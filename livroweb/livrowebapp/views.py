c2068e6da075532016ab662c8fd03ef511d427ca
rejgn <rmtgallego@gmail.com>
2023-09-30 14:37:18 +0800

    Reader or writer layout

--- livroweb/livrowebapp/views.py
+++ livroweb/livrowebapp/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def land(request):
-    return render(request, 'livrowebapp/landing.html')
+    return render(request, 'livrowebapp/readwrite.html')
 def signin(request):
     return render(request, 'livrowebapp/signin.html')
