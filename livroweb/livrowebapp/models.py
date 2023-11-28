ca81daf1573ea7ec237b9c90a8221334cd019b28
Andrian <mabinas@tip.edu.ph>
2023-11-28 12:55:30 +0800

    OKI NA

--- livroweb/livrowebapp/models.py
+++ livroweb/livrowebapp/models.py
@@ -5,7 +5,7 @@ class Member(models.Model):
     email = models.EmailField(max_length=30)
     password = models.CharField(max_length=16)
     type_user = models.CharField(max_length=10)
-    active = models.BooleanField(default=True) 
+    #active = models.BooleanField(default=True) 
     
     def __str__(self):
         return self.username
--- livroweb/livrowebapp/views.py
+++ livroweb/livrowebapp/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.shortcuts import render, redirect
 from .models import Member
 from .forms import Memberform
@@ -6,7 +7,24 @@ from django.contrib import messages
 def land(request):
     return render(request, 'livrowebapp/landing.html')
 def signin(request):
-    return render(request, 'livrowebapp/signin.html')
+    if request.method == "GET":
+        mail = request.GET.get('mail')
+        passw = request.GET.get('pass')
+        now = datetime.now()
+        hour = now.hour
+        greeting = 'Good Morning' if 5 <= hour < 12 else ('Good afternoon' if 12 <= hour < 18 else 'Good Evening')
+        try:
+            member = Member.objects.get(email=mail, password=passw)
+            if member.type_user == 'Reader':
+                messages.success(request, f'{greeting}, {member.username}!')
+                return redirect('browse_reader')
+            elif member.type_user == 'Writer':
+                messages.success(request, f'{greeting}, {member.username}!')
+                return redirect('browse_writer')
+        except Member.DoesNotExist:
+            return render(request, 'livrowebapp/signin.html')
+    else:
+        return render(request, 'livrowebapp/signin.html')
 def signup(request):
     if request.method == "POST":
         form = Memberform(request.POST or None)
