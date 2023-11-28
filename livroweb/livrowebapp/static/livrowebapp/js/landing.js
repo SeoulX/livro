122faef3c0b44b8015a0ed5315b93c304ead8d26
mcllontac <248klontac@gmail.com>
2023-11-28 23:59:51 +0800

    landing popup for signin and signup

---
+++ livroweb/livrowebapp/static/livrowebapp/js/landing.js
@@ -0,0 +1,74 @@
+document.addEventListener('DOMContentLoaded', function () {
+    function openPopup(url) {
+        fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            var popupContainer = document.getElementById('popup-container');
+            popupContainer.innerHTML = data;
+            popupContainer.style.display = 'flex';
+            popupContainer.classList.add('overlay');  // Add the overlay class
+        })
+        .catch(error => console.error('Error fetching content:', error));
+}
+
+    function closePopup() {
+        var popupContainer = document.getElementById('popup-container');
+        popupContainer.innerHTML = '';  // Clear the content when closing
+        popupContainer.style.display = 'none';
+        popupContainer.classList.remove('overlay');
+    }
+
+    document.getElementById('login-button').addEventListener('click', function () {
+        openPopup('/signin/');
+    });
+
+    document.getElementById('signup-button').addEventListener('click', function () {
+        openPopup('/signup/');
+    });
+
+    function closeForm() {
+        closePopup();
+    }
+
+    var closeButtons = document.getElementsByClassName('closebutton');
+    for (var i = 0; i < closeButtons.length; i++) {
+        closeButtons[i].addEventListener('click', closeForm);
+    }
+
+
+    window.addEventListener('popstate', function (event) {
+        closePopup();
+    });
+
+});
+
+function closeSignIn() {
+    document.querySelector('.wrap').style.display = 'none';
+    removeOverlay();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var closeButton = document.querySelector('.closebutton');
+    if (closeButton) {
+        closeButton.addEventListener('click', closeSignIn);
+    }
+});
+
+function closeSignUp() {
+    document.querySelector('.popup').style.display = 'none';
+    removeOverlay();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var closeButton = document.querySelector('.close-btn');
+    if (closeButton) {
+        closeButton.addEventListener('click', closeSignUp);
+    }
+});
+
+function removeOverlay() {
+    var popupContainer = document.getElementById('popup-container');
+    popupContainer.innerHTML = '';  // Clear the content when closing
+    popupContainer.style.display = 'none';
+    popupContainer.classList.remove('overlay');  // Remove the overlay class
+}
