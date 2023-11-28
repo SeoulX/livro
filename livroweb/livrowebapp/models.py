0fbae9ad39cb4d4e1ab0b1a8ca0777d26d706ed3
mcllontac <248klontac@gmail.com>
2023-11-28 09:47:04 +0800

    fix post and get of signin and signup

--- livroweb/livrowebapp/models.py
+++ livroweb/livrowebapp/models.py
@@ -3,8 +3,9 @@ from django.db import models
 class Member(models.Model):
     username = models.CharField(max_length=20)
     email = models.EmailField(max_length=30)
-    password = models.CharField(max_length=10)
+    password = models.CharField(max_length=16)
     type_user = models.CharField(max_length=10)
+    active = models.BooleanField(default=True) 
     
     def __str__(self):
         return self.username
