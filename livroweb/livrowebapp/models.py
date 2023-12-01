f106aeabb037d9b0bc2e2b392243f2ae8f91bc0b
papiipapz <jessdrewpigg@gmail.com>
2023-12-01 19:35:41 +0800

    bootstraplink.css

--- livroweb/livrowebapp/models.py
+++ livroweb/livrowebapp/models.py
@@ -5,38 +5,30 @@ class Member(models.Model):
     email = models.EmailField(max_length=30)
     password = models.CharField(max_length=16)
     type_user = models.CharField(max_length=10)
-    #active = models.BooleanField(default=True) 
-    #member can have multiple libraries
-    libraries = models.ManyToManyField('Library')
+    libraries = models.ManyToManyField('Library', related_name='members')
     
     def __str__(self):
         return self.username
     
-# on_delete=models.CASCADE means that if a value is deleted, all its related data will be deleted as well (ata i think)
-    
 class Library(models.Model):
     library_name = models.CharField(max_length=200)
     book_count = models.IntegerField()
-    # Assuming we want to establish a one-to-many relationship between Library and Book
-    # A Library can have multiple books, but each book belongs to only one library
-    book = models.ForeignKey('Book', on_delete=models.CASCADE)
-    #library can have multiple books
-    books = models.ManyToManyField('Book')
-
+    # Assuming a Library can have multiple books and each book belongs to only one library
+    # Renaming the ForeignKey and ManyToManyField relationships
+    main_book = models.ForeignKey('Book', on_delete=models.CASCADE, related_name='main_libraries')
+    books = models.ManyToManyField('Book', related_name='libraries')
+    
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     genre = models.CharField(max_length=100)
     description = models.TextField()
     book_cover = models.ImageField(upload_to='book_covers/')
-    #book can have multiple feedbacks
-    feedbacks = models.ManyToManyField('Feedback')
-
+    feedbacks = models.ManyToManyField('Feedback', related_name='books')
+    
 class Feedback(models.Model):
     user = models.ForeignKey(Member, on_delete=models.CASCADE)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='feedback_entries')
     likes = models.PositiveIntegerField()
     dislikes = models.PositiveIntegerField()
     comments = models.TextField()
-    
-    
